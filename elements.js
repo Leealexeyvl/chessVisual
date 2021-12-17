f01a61b1173cc8a25b17ed14c1321a9fc9b71cd9
Alex <40343142+Leealexeyvl@users.noreply.github.com>
2021-12-17 23:27:35 +0300

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,30 @@
+// area with piece icon
+export const currentPieceCharacterOnBoard = document.getElementById("piece");
+export const previousPieceButton = document.getElementById("piece-left");
+export const nextPieceButton = document.getElementById("piece-right");
+export const currentPiecePositionOnBoard =
+  document.getElementById("piece-position");
+
+// area with piece amount
+export const currentPieceNumberOnBoard = document.getElementById(
+  "pieces-amount-current"
+);
+export const totalAmountOfPiecesOnBoard = document.getElementById(
+  "pieces-amount-total"
+);
+
+// area with puzzles amount
+export const currentPuzzleNumber = document.getElementById(
+  "puzzles-current-number"
+);
+export const totalAmountOfPuzzles = document.getElementById(
+  "puzzles-total-number"
+);
+export const previousPuzzleButton = document.getElementById("puzzle-left");
+export const nextPuzzleButton = document.getElementById("puzzle-right");
+
+// input area
+export const result = document.querySelector("#result");
+export const guess = document.querySelector("#guess");
+
+// result
---
+++ script.js
@@ -0,0 +1,97 @@
+import { PUZZLES, PIECES_UNICODE, ANSWERS } from "./consts.js";
+import * as elements from "./elements.js";
+
+// global variables
+let currentPieceInArrayOfCurrentPuzzle = 0;
+let currentPuzzleNumberInArrayOfPuzzles = 0;
+let totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
+let totalPuzzlesInGame = PUZZLES.length;
+
+// init every value on page
+const initialAllValues = () => {
+  elements.currentPieceCharacterOnBoard.textContent =
+    PIECES_UNICODE[
+      PUZZLES[currentPuzzleNumberInArrayOfPuzzles][
+        currentPieceInArrayOfCurrentPuzzle
+      ][0]
+    ];
+  elements.currentPiecePositionOnBoard.textContent =
+    PUZZLES[currentPuzzleNumberInArrayOfPuzzles][
+      currentPieceInArrayOfCurrentPuzzle
+    ][1];
+  elements.currentPieceNumberOnBoard.textContent =
+    currentPieceInArrayOfCurrentPuzzle + 1;
+  elements.totalAmountOfPiecesOnBoard.textContent = totalPiecesOnBoard;
+  elements.currentPuzzleNumber.textContent =
+    currentPuzzleNumberInArrayOfPuzzles + 1;
+  elements.totalAmountOfPuzzles.textContent = totalPuzzlesInGame;
+  elements.result.textContent = "";
+  elements.guess.value = "";
+};
+
+initialAllValues();
+
+// move forward through chess pieces in current puzzle
+elements.nextPieceButton.addEventListener("click", () => {
+  // piece icon and position increment
+  currentPieceInArrayOfCurrentPuzzle++;
+  if (currentPieceInArrayOfCurrentPuzzle > totalPiecesOnBoard - 1) {
+    currentPieceInArrayOfCurrentPuzzle = 0;
+  }
+
+  // update all values
+  initialAllValues();
+});
+
+// move back through chess pieces in current puzzle
+elements.previousPieceButton.addEventListener("click", () => {
+  // piece character and position decrement
+  currentPieceInArrayOfCurrentPuzzle--;
+  if (currentPieceInArrayOfCurrentPuzzle < 0) {
+    currentPieceInArrayOfCurrentPuzzle = totalPiecesOnBoard - 1;
+  }
+
+  // update all values
+  initialAllValues();
+});
+
+// move forward through chess pieces for current puzzle
+elements.nextPuzzleButton.addEventListener("click", () => {
+  // piece character and position increment
+  currentPuzzleNumberInArrayOfPuzzles++;
+  if (currentPuzzleNumberInArrayOfPuzzles > totalPuzzlesInGame - 1) {
+    currentPuzzleNumberInArrayOfPuzzles = 0;
+  }
+
+  // update global variables
+  totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
+  currentPieceInArrayOfCurrentPuzzle = 0;
+
+  // update all values
+  initialAllValues();
+});
+
+// move back through puzzles
+elements.previousPuzzleButton.addEventListener("click", () => {
+  currentPuzzleNumberInArrayOfPuzzles--;
+  if (currentPuzzleNumberInArrayOfPuzzles < 0) {
+    currentPuzzleNumberInArrayOfPuzzles = totalPuzzlesInGame - 1;
+  }
+
+  // update global variables
+  totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
+  currentPieceInArrayOfCurrentPuzzle = 0;
+
+  // update all values
+  initialAllValues();
+});
+
+// click check answer
+document.querySelector("#submit-answer").addEventListener("click", () => {
+  if (elements.guess.value === ANSWERS[currentPuzzleNumberInArrayOfPuzzles]) {
+    elements.result.textContent = "Correct!";
+  } else {
+    elements.result.textContent = "Wrong!";
+  }
+  elements.guess.value = "";
+});
