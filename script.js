a33c7a1a430eb37ff9bd60a7fba3d3f14703b140
alli <alli@ozon.ru>
2022-10-17 22:11:01 +0300

    refactor code with functions

--- script.js
+++ script.js
@@ -20,15 +20,95 @@ let audio = new Audio();
 let soundNumber = 0;
 
 // init every value on page
-const initialAllValues = () => {
-  elements.currentPieceCharacterOnBoard.setAttribute(
-    "src",
-    PIECES_SRC[
-      PUZZLES[currentPuzzleNumberInArrayOfPuzzles][
-        currentPieceInArrayOfCurrentPuzzle
-      ][0]
-    ]
-  );
+initialAllValues();
+
+// move forward through chess pieces in current puzzle
+elements.nextPieceButton.addEventListener("click", () => {
+  currentPieceInArrayOfCurrentPuzzle++;
+  if (currentPieceInArrayOfCurrentPuzzle > totalPiecesOnBoard - 1) {
+    currentPieceInArrayOfCurrentPuzzle = 0;
+  }
+  initialAllValues();
+});
+
+// move back through chess pieces in current puzzle
+elements.previousPieceButton.addEventListener("click", () => {
+  currentPieceInArrayOfCurrentPuzzle--;
+  if (currentPieceInArrayOfCurrentPuzzle < 0) {
+    currentPieceInArrayOfCurrentPuzzle = totalPiecesOnBoard - 1;
+  }
+  initialAllValues();
+});
+
+// move forward through puzzles
+elements.nextPuzzleButton.addEventListener("click", () => {
+  currentPuzzleNumberInArrayOfPuzzles++;
+  if (currentPuzzleNumberInArrayOfPuzzles > totalPuzzlesInGame - 1) {
+    currentPuzzleNumberInArrayOfPuzzles = 0;
+  }
+  totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
+  currentPieceInArrayOfCurrentPuzzle = 0;
+  initialAllValues();
+});
+
+// move back through puzzles
+elements.previousPuzzleButton.addEventListener("click", () => {
+  currentPuzzleNumberInArrayOfPuzzles--;
+  if (currentPuzzleNumberInArrayOfPuzzles < 0) {
+    currentPuzzleNumberInArrayOfPuzzles = totalPuzzlesInGame - 1;
+  }
+  totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
+  currentPieceInArrayOfCurrentPuzzle = 0;
+  initialAllValues();
+});
+
+// check answer
+document.querySelector("#submit-answer").addEventListener("click", () => {
+  if (elements.guess.value === ANSWERS[currentPuzzleNumberInArrayOfPuzzles]) {
+    elements.result.textContent = "Correct!";
+  } else {
+    elements.result.textContent = "Wrong!";
+  }
+  elements.guess.value = "";
+});
+
+// audio playing
+elements.listen.addEventListener("click", () => {
+  initialAllValues();
+  startPlayingAudio();
+});
+
+elements.listen.addEventListener("touchstart", () => {
+  initialAllValues();
+  startPlayingAudio();
+});
+
+audio.addEventListener("ended", () => {
+  soundNumber++;
+  if (soundNumber >= pathAudio.length) {
+    soundNumber = 0;
+    return;
+  }
+  startPlayingAudio();
+});
+
+// delete double click
+document.body.addEventListener("dblclick", (event) => {
+  event.preventDefault();
+});
+
+// functions
+function initialAllValues() {
+  initPiecePicture();
+  initPiecePosition();
+  initNumbers();
+  initResult();
+  createAudioArr();
+  stopPlayingAudio();
+  formAudioPath();
+}
+
+function createAudioArr() {
   soundNumber = 0;
   audioArr = [];
   audioArr.push(
@@ -46,9 +126,41 @@ const initialAllValues = () => {
       currentPieceInArrayOfCurrentPuzzle
     ][1][1]
   );
-  console.log(audioArr);
-  audio.pause();
-  audio.currentTime = 0;
+}
+
+function initPiecePicture() {
+  elements.currentPieceCharacterOnBoard.setAttribute(
+    "src",
+    PIECES_SRC[
+      PUZZLES[currentPuzzleNumberInArrayOfPuzzles][
+        currentPieceInArrayOfCurrentPuzzle
+      ][0]
+    ]
+  );
+}
+
+function initPiecePosition() {
+  elements.currentPiecePositionOnBoard.textContent =
+    PUZZLES[currentPuzzleNumberInArrayOfPuzzles][
+      currentPieceInArrayOfCurrentPuzzle
+    ][1];
+}
+
+function initNumbers() {
+  elements.currentPieceNumberOnBoard.textContent =
+    currentPieceInArrayOfCurrentPuzzle + 1;
+  elements.totalAmountOfPiecesOnBoard.textContent = totalPiecesOnBoard;
+  elements.currentPuzzleNumber.textContent =
+    currentPuzzleNumberInArrayOfPuzzles + 1;
+  elements.totalAmountOfPuzzles.textContent = totalPuzzlesInGame;
+}
+
+function initResult() {
+  elements.result.textContent = "";
+  elements.guess.value = "";
+}
+
+function formAudioPath() {
   pathAudio = [];
   if (audioArr[0].toLowerCase() === audioArr[0]) {
     pathAudio.push(PATH_LIST_COLORS.black);
@@ -59,116 +171,14 @@ const initialAllValues = () => {
   pathAudio.push(PATH_LIST_PIECES[audioArr[0].toLowerCase()]);
   pathAudio.push(PATH_LIST_LETTERS[audioArr[1]]);
   pathAudio.push(PATH_LIST_NUMBERS[audioArr[2]]);
+}
 
-  elements.currentPiecePositionOnBoard.textContent =
-    PUZZLES[currentPuzzleNumberInArrayOfPuzzles][
-      currentPieceInArrayOfCurrentPuzzle
-    ][1];
-  elements.currentPieceNumberOnBoard.textContent =
-    currentPieceInArrayOfCurrentPuzzle + 1;
-  elements.totalAmountOfPiecesOnBoard.textContent = totalPiecesOnBoard;
-  elements.currentPuzzleNumber.textContent =
-    currentPuzzleNumberInArrayOfPuzzles + 1;
-  elements.totalAmountOfPuzzles.textContent = totalPuzzlesInGame;
-  elements.result.textContent = "";
-  elements.guess.value = "";
-};
-
-initialAllValues();
-
-// delete double click
-document.body.addEventListener("dblclick", (event) => {
-  event.preventDefault();
-});
-
-// move forward through chess pieces in current puzzle
-elements.nextPieceButton.addEventListener("click", () => {
-  // piece icon and position increment
-  currentPieceInArrayOfCurrentPuzzle++;
-  if (currentPieceInArrayOfCurrentPuzzle > totalPiecesOnBoard - 1) {
-    currentPieceInArrayOfCurrentPuzzle = 0;
-  }
-
-  // update all values
-  initialAllValues();
-});
-
-// move back through chess pieces in current puzzle
-elements.previousPieceButton.addEventListener("click", () => {
-  // piece character and position decrement
-  currentPieceInArrayOfCurrentPuzzle--;
-  if (currentPieceInArrayOfCurrentPuzzle < 0) {
-    currentPieceInArrayOfCurrentPuzzle = totalPiecesOnBoard - 1;
-  }
-
-  // update all values
-  initialAllValues();
-});
-
-// move forward through puzzles
-elements.nextPuzzleButton.addEventListener("click", () => {
-  // piece character and position increment
-  currentPuzzleNumberInArrayOfPuzzles++;
-  if (currentPuzzleNumberInArrayOfPuzzles > totalPuzzlesInGame - 1) {
-    currentPuzzleNumberInArrayOfPuzzles = 0;
-  }
-
-  // update global variables
-  totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
-  currentPieceInArrayOfCurrentPuzzle = 0;
-
-  // update all values
-  initialAllValues();
-});
-
-// move back through puzzles
-elements.previousPuzzleButton.addEventListener("click", () => {
-  currentPuzzleNumberInArrayOfPuzzles--;
-  if (currentPuzzleNumberInArrayOfPuzzles < 0) {
-    currentPuzzleNumberInArrayOfPuzzles = totalPuzzlesInGame - 1;
-  }
-
-  // update global variables
-  totalPiecesOnBoard = PUZZLES[currentPuzzleNumberInArrayOfPuzzles].length;
-  currentPieceInArrayOfCurrentPuzzle = 0;
-
-  // update all values
-  initialAllValues();
-});
-
-// click check answer
-document.querySelector("#submit-answer").addEventListener("click", () => {
-  if (elements.guess.value === ANSWERS[currentPuzzleNumberInArrayOfPuzzles]) {
-    elements.result.textContent = "Correct!";
-  } else {
-    elements.result.textContent = "Wrong!";
-  }
-  elements.guess.value = "";
-});
-
-// audio playing
-
-audio.src = pathAudio[soundNumber];
-
-audio.addEventListener("ended", () => {
-  console.log("End");
-  soundNumber++;
-  if (soundNumber >= pathAudio.length) {
-    soundNumber = 0;
-    return;
-  }
+function startPlayingAudio() {
   audio.src = pathAudio[soundNumber];
   audio.play();
-});
-elements.listen.addEventListener("click", () => {
-  initialAllValues();
-  audio.src = pathAudio[soundNumber];
-  audio.play();
-  console.log("Start");
-});
-elements.listen.addEventListener("touchstart", () => {
-  initialAllValues();
-  audio.src = pathAudio[soundNumber];
-  audio.play();
-  console.log("Start");
-});
+}
+
+function stopPlayingAudio() {
+  audio.pause();
+  audio.currentTime = 0;
+}
